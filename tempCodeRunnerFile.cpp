8fda9555f382f8b390e7def5a3e688db276f51f1
Aditya-Mishra19 <skadityamishra@gmail.com>
2023-01-28 12:24:34 +0530

    Fibonacci dp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -15,34 +15,22 @@ int fib(int n, vector<int> &dp) {
     if(dp[n] != -1) return dp[n];
     return dp[n] = fib(n-1, dp) + fib(n-2, dp);
 
-}
-
-*/
+}*/
 
 //Optimized space 0(1)
 int fib(int n) {
-    int prev2= 0;
-    int prev = 1;
+    if(n <= 1) return n;
+    int prev= 0;
+    int prev2 = 1;
     for(int i=2; i<=n; i++){
-        int curri = prev2 + prev;
+        int curri = prev + prev2;
         prev2 = prev;
         prev = curri;
     }
     return prev;
 }
-
 int main() {
     int n;
     cout << "Enter n: " << endl;
     cin >> n;
     vector<int> dp(n+1, -1);
-    cout << "Fib is: " << fib(n);
-    //Tabulation
-    // dp[0] = 0;
-    // dp[1] = 1;
-    // for(int i=2; i<=n; i++){
-    //     dp[i]= dp[i-1] + dp[i-2];
-    // }
-    // cout<<dp[n];
-    return 0;
-}
